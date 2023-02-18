c006d28c5a36061cb9fabfb2c842d737a89d1d11
zzdpk2 <zzdpk2@126.com>
2023-02-19 02:43:14 +0800

    add new file

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func user(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, user is %s!", r.URL.Path[1:])
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/", user)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
