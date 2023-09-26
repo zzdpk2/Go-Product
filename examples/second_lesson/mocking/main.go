4255d5deeae01932e410c4854b9f2ff01547e2fa
zzdpk2 <zzdpk2@126.com>
2023-09-27 04:10:11 +1000

    add new files

--- examples/second_lesson/mocking/main.go
+++ examples/second_lesson/mocking/main.go
@@ -30,19 +30,28 @@ func (o *ConfigurableSleeper) Sleep() {
 	time.Sleep(o.duration)
 }
 
+const (
+	sleep = "sleep"
+	write = "write"
+)
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func main() {
 	sleeper := &ConfigurableSleeper{duration: 1 * time.Second}
 	Countdown(os.Stdout, sleeper)
 }
-
-// func Countdown(out io.Writer, sleeper Sleeper) {
-// 	for i := countdownStart; i > 0; i-- {
-// 		sleeper.Sleep()
-// 		fmt.Fprintln(out, i)
-// 	}
-// 	sleeper.Sleep()
-// 	fmt.Fprintf(out, finalWord)
-// }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
@@ -56,3 +65,12 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	sleeper.Sleep()
 	fmt.Fprintln(out, finalWord)
 }
+
+// func Countdown(out io.Writer, sleeper Sleeper) {
+// 	for i := countdownStart; i > 0; i-- {
+// 		sleeper.Sleep()
+// 		fmt.Fprintln(out, i)
+// 	}
+// 	sleeper.Sleep()
+// 	fmt.Fprintf(out, finalWord)
+// }
