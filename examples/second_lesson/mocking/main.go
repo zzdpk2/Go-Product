2165797ae7c309cd6594622904abdcf7eeca3e11
zzdpk2 <zzdpk2@126.com>
2023-09-26 17:00:07 +1000

    add new code

--- examples/second_lesson/mocking/main.go
+++ examples/second_lesson/mocking/main.go
@@ -1,1 +1,58 @@
-package mocking
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{duration: 1 * time.Second}
+	Countdown(os.Stdout, sleeper)
+}
+
+// func Countdown(out io.Writer, sleeper Sleeper) {
+// 	for i := countdownStart; i > 0; i-- {
+// 		sleeper.Sleep()
+// 		fmt.Fprintln(out, i)
+// 	}
+// 	sleeper.Sleep()
+// 	fmt.Fprintf(out, finalWord)
+// }
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprintln(out, finalWord)
+}
